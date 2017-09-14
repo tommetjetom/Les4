efa4653d98f7d369b860c917374588359bca8f2c
Tommy <tom.monshouwer@student.hu.nl>
2017-09-14 21:38:27 +0200

    test

---
+++ Les 2/Pe 2.2.py
@@ -0,0 +1,8 @@
+cijferPROG= 7
+cijferICOR= 8
+cijferCSN= 6
+cijferTOT= (cijferCSN + cijferICOR + cijferPROG)
+cijferGEM= (cijferTOT/ 3)
+cijferbeloning= (cijferTOT * 30)
+print("Je hebt een gemiddeldcijfer van " , cijferGEM)
+print("Je hebt in totaal ", cijferTOT ,"punten behaald en hiermee ", cijferbeloning ,"euro mee verdiend.")
